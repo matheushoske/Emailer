f4002d8152bf49791ecd60d3eaa9d99456bb16e2
matheushoske <matheushoske@gmail.com>
2022-03-18 19:56:28 -0300

    Updates

--- Email.cs
+++ Email.cs
@@ -9,11 +9,8 @@
 // Be Welcome to Fork with this project
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Emailer
 {
